7d453012c1c284524e8be2413f30307a41a521cd
Aaron Lee <aaron@test.com>
2021-04-13 20:27:50 +0100

    Gradle build works. Set up game-logic project

---
+++ game-logic/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    implementation("org.scala-lang:scala-library:2.13.3")
+
+    testImplementation("junit:junit:4.13.1")
+    testImplementation("org.scalatest:scalatest_2.13:3.2.3")
+    testImplementation("org.scalatestplus:junit-4-13_2.13:3.2.2.0")
+}

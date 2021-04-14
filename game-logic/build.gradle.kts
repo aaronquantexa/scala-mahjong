036fb81bf630d6051aea1ae85a78e5a909abb742
Aaron Lee <aaron@test.com>
2021-04-14 23:31:42 +0100

    Standardise scala version. Add gradle scalatest plugin

--- game-logic/build.gradle.kts
+++ game-logic/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     scala
+    id("com.github.maiflai.scalatest") version "0.30"
 }
 
 repositories {
@@ -7,8 +8,9 @@ repositories {
 }
 
 dependencies {
-    implementation("org.scala-lang:scala-library:2.13.3")
+    implementation("org.scala-lang:scala-library:2.13.5")
     implementation(project(":tile-model"))
 
     testImplementation("org.scalatest:scalatest_2.13:3.2.7")
+    testRuntimeOnly ("com.vladsch.flexmark:flexmark-all:0.35.10")
 }
--- tile-model/build.gradle.kts
+++ tile-model/build.gradle.kts
@@ -7,5 +7,5 @@ repositories {
 }
 
 dependencies {
-    implementation("org.scala-lang:scala-library:2.13.3")
+    implementation("org.scala-lang:scala-library:2.13.5")
 }
